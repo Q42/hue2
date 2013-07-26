f65f7901343a3feaf1f3b4b138f368056dcca853
Alexander Overvoorde <overv161@gmail.com>
2013-07-26 12:01:56 +0200

    Fixed switch checked bug

--- hue2/src/main/java/nl/q42/hue2/activities/LightsActivity.java
+++ hue2/src/main/java/nl/q42/hue2/activities/LightsActivity.java
@@ -21,7 +21,6 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
